18e6d06d336cc16a3ab9c33ed6b0769846566767
REALM Robot WS <realm-robot-ws@mit.edu>
2022-01-11 11:12:19 -0500

    IMU + scan not great for odometry. Switching to slam

--- architect/examples/agv_localization/agv_ros_controller.py
+++ architect/examples/agv_localization/agv_ros_controller.py
@@ -4,19 +4,27 @@ from typing import Optional, Tuple
 
 import rospy
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu, LaserScan
 import tf
 from tf.transformations import euler_from_quaternion
 
 import jax
 import jax.numpy as jnp
 import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.signal
+import scipy.spatial
 
 from architect.components.dynamics.dubins import (
     dubins_next_state,
     dubins_linearized_dynamics,
 )
 from architect.components.estimation.ekf import dt_ekf_predict_covariance, dt_ekf_update
-from .agv_simulator import (
+from architect.components.geometry.transforms_2d import rotation_matrix_2d
+from architect.components.sensing.range_beacons import (
+    beacon_range_measurements,
+)
+from architect.examples.agv_localization.agv_simulator import (
     navigate,
     navigation_function,
     get_observations,
@@ -57,6 +65,18 @@ class AGVROSController(object):
         # create a publisher node to send velocity commands to turtlebot
         self.command_publisher = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
+        # Subscribe to the IMU topic to get the heading angle
+        self.heading_angle = None
+        self.heading_offset = None
+        rospy.Subscriber("/imu", Imu, self.imu_callback)
+
+        # Subscribe to the lidar scan to get the xy position
+        # (from which we fake beacons)
+        self.ranges = None
+        self.angles = None
+        self.p_WT = None
+        rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+
         # Make sure to stop the turtlebot using a callback when this node gets shut down
         rospy.on_shutdown(self.on_shutdown)
 
@@ -84,16 +104,16 @@ class AGVROSController(object):
         # Navigation Initialization
         # ------------------------------
 
-        # When the turtlebot wakes up, it's odometry thinks that it is at the origin.
-        # We can fake starting at some random position by adding a random offset
+        # Get the starting odometry measurement and find an offset that puts it at
+        # the intended starting location
+        (trans, rot) = self.listener.lookupTransform(
+            self.odom_frame, self.base_frame, rospy.Time(0)
+        )
+        rotation = euler_from_quaternion(rot)
+        initial_odometry_state = jnp.array([trans[0], trans[1], rotation[2]])
         initial_state_mean = jnp.array([-2.2, 0.5, 0.0])
         initial_state_covariance = 0.001 * jnp.eye(3)
-        self.prng_key, subkey = jax.random.split(self.prng_key)
-        self.state_offset = jax.random.multivariate_normal(
-            subkey,
-            initial_state_mean,
-            initial_state_covariance,
-        )
+        self.state_offset = initial_odometry_state - initial_state_mean
 
         # Initialize the state estimate and error covariance for the EKF
         self.state_est_mean = initial_state_mean
@@ -114,7 +134,9 @@ class AGVROSController(object):
         self.command_publisher.publish(Twist())
         rospy.sleep(1.0)
 
-        self.log_df.to_csv(f"agv_controller_log_{round(rospy.get_time(), 2)}.csv")
+        self.log_df.to_csv(f"agv_controller_log.csv")
+
+        # Also plot the data to see what happened
 
     def step(self) -> None:
         """
@@ -122,9 +144,9 @@ class AGVROSController(object):
         """
         # Get the current observations
         observations = self.get_observations()
-        # Update the state estimate using the EKF
-        if observations is not None:
-            self.state_est_mean, self.state_est_cov = self.ekf_update(observations)
+        # # Update the state estimate using the EKF
+        # if observations is not None:
+        #     self.state_est_mean, self.state_est_cov = self.ekf_update(observations)
 
         # Get the control command based on the current state estimate
         # (this also returns the navigation function value at this point)
@@ -135,24 +157,84 @@ class AGVROSController(object):
         base_log_packet = {"time": rospy.get_time()}
         log_packet = copy(base_log_packet)
         log_packet["measurement"] = "state_est_mean"
-        log_packet["value"] = self.state_est_mean
+        log_packet["value"] = self.state_est_mean.tolist()
         self.log_df = self.log_df.append(log_packet, ignore_index=True)
         log_packet["measurement"] = "state_est_cov"
-        log_packet["value"] = self.state_est_cov
+        log_packet["value"] = self.state_est_cov.tolist()
         self.log_df = self.log_df.append(log_packet, ignore_index=True)
         log_packet["measurement"] = "control_command"
-        log_packet["value"] = control_command
+        log_packet["value"] = control_command.tolist()
         self.log_df = self.log_df.append(log_packet, ignore_index=True)
         log_packet["measurement"] = "V"
         log_packet["value"] = V
         self.log_df = self.log_df.append(log_packet, ignore_index=True)
 
-        # Predict the next state
-        self.state_est_mean, self.state_est_cov = self.ekf_predict(control_command)
+        # # Predict the next state
+        # self.state_est_mean, self.state_est_cov = self.ekf_predict(control_command)
 
         # Execute the control command and hold for the desired frequency
         self.execute_command(control_command)
         self.rate.sleep()
+
+    def imu_callback(self, data):
+        # Extract the orientation and convert it to a heading angle
+        orientation_quat = data.orientation
+        orientation_quat = [
+            orientation_quat.x,
+            orientation_quat.y,
+            orientation_quat.z,
+            orientation_quat.w,
+        ]
+        orientation_euler = euler_from_quaternion(orientation_quat)
+
+        # If we have not saved an offset, save this reading as the offset
+        if self.heading_offset is None:
+            self.heading_offset = orientation_euler[2]
+
+        # Save the heading angle accounting for the offset
+        self.heading_angle = orientation_euler[2] - self.heading_offset
+
+    def scan_callback(self, data):
+        # We don't actually have beacons installed, so we have to fake them by
+        # using Lidar to find our location in a rectangular arena and use that
+        # state to compute what the beacon measurement would be (with noise)
+
+        # Start by getting the lidar measurements
+        angles = jnp.arange(
+            data.angle_min, data.angle_max + data.angle_increment, data.angle_increment
+        )
+        ranges = jnp.array(data.ranges)
+
+        # Get valid points
+        valid = ranges > 0.0
+        angles = angles[valid]
+        ranges = ranges[valid]
+
+        self.angles = angles
+        self.ranges = ranges
+
+        # Convert to cartesian
+        x = self.ranges * jnp.cos(self.angles)
+        y = self.ranges * jnp.sin(self.angles)
+
+        # Rotate to world frame using the estimated angle
+        try:
+            R_WT = rotation_matrix_2d(self.state_est_mean[2])
+            R_TW = R_WT.T
+        except AttributeError:
+            return  # happens if called before state_est_mean is set
+
+        p_TurtleLidar = jnp.vstack((x, y))
+        p_WLidar = R_TW @ p_TurtleLidar
+        p_WLidar = p_WLidar.T
+
+        # Get the upper right corner of the box relative to the turtle
+        p_TCorner_W = jnp.array([p_WLidar[:, 0].max(), p_WLidar[:, 1].max()])
+
+        # Hand-measured from box origin to corner
+        p_WCorner = jnp.array([0.38, 0.83])
+
+        self.p_WT = p_WCorner - p_TCorner_W
 
     def get_observations(self) -> Optional[jnp.ndarray]:
         """Get the observations from beacons. We don't actually have radio beacons
@@ -162,21 +244,25 @@ class AGVROSController(object):
             (self.beacon_locations.shape[0] + 1,) vector of ranges to beacons, or None
             if no data is available.
         """
-        # Get the location of the robot as estimated by the latest available odometry
-        try:
-            (trans, rot) = self.listener.lookupTransform(
-                self.odom_frame, self.base_frame, rospy.Time(0)
-            )
-            rotation = euler_from_quaternion(rot)
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException) as e:
-            rospy.loginfo(f"tf Exception:\n{e}")
-            return None
-
-        state = jnp.array([trans[0], trans[1], rotation[2]])
-
-        return get_observations(
-            state, self.beacon_locations, jnp.zeros(self.beacon_locations.shape[0] + 1)
+        # Fake the beacon measurements by getting the state from odometry
+        (trans, rot) = self.listener.lookupTransform(
+            self.odom_frame, self.base_frame, rospy.Time(0)
         )
+        rotation = euler_from_quaternion(rot)
+        odometry_state = jnp.array([trans[0], trans[1], rotation[2]])
+        odometry_state_corrected = odometry_state - self.state_offset
+        self.state_est_mean = odometry_state_corrected
+        print(self.state_est_mean)
+
+        ranges = beacon_range_measurements(
+            odometry_state_corrected[:2],
+            self.beacon_locations,
+            jnp.zeros(self.beacon_locations.shape[0]),
+        )
+
+        heading = jnp.array([rotation[2]])
+
+        return jnp.concatenate((heading, ranges))
 
     def compute_control_command(self) -> Tuple[jnp.ndarray, float]:
         """Compute the control command and navigation function value given the current
---
+++ architect/examples/agv_localization/plot_ros_results.py
@@ -0,0 +1,32 @@
+import ast
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import jax
+import jax.numpy as jnp
+
+from architect.examples.agv_localization.agv_simulator import navigation_function
+
+
+if __name__ == "__main__":
+    filename = "agv_controller_log.csv"
+    df = pd.read_csv(filename)
+    state_estimates = df[df.measurement == "state_est_mean"].value.apply(
+        lambda x: ast.literal_eval(x)
+    )
+    state_estimates = list(state_estimates)
+    state_estimates = jnp.array(state_estimates)
+
+    plt.plot(state_estimates[:, 0], state_estimates[:, 1], "o-")
+
+    # Plot the navigation function
+    x = jnp.linspace(-3, 0, 100)
+    y = jnp.linspace(-1, 1, 100)
+
+    X, Y = jnp.meshgrid(x, y)
+    XY = jnp.stack((X, Y)).reshape(2, 10000).T
+
+    V = jax.vmap(navigation_function, in_axes=0)(XY).reshape(100, 100)
+    contours = plt.contourf(X, Y, V, levels=10)
+
+    plt.show()
