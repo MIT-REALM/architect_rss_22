e4f4dcde3dda9c01eb85cbdb6fa789e82dbb8565
Charles Dawson <charles.dwsn@gmail.com>
2022-02-16 14:35:48 -0500

    Add new STL components to the init file

--- architect/components/specifications/stl/__init__.py
+++ architect/components/specifications/stl/__init__.py
@@ -1,7 +1,31 @@
-from .formula import STLFormula
+from .formula import (
+    STLFormula,
+    STLPredicate,
+    STLNegation,
+    STLAnd,
+    STLOr,
+    STLImplies,
+    STLUntimedEventually,
+    STLTimedEventually,
+    STLUntimedUntil,
+    STLTimedUntil,
+    STLUntimedAlways,
+    STLTimedAlways,
+)
 from .signal import SampledSignal
 
 __all__ = [
     "SampledSignal",
     "STLFormula",
+    "STLPredicate",
+    "STLNegation",
+    "STLAnd",
+    "STLOr",
+    "STLImplies",
+    "STLUntimedEventually",
+    "STLTimedEventually",
+    "STLUntimedUntil",
+    "STLTimedUntil",
+    "STLUntimedAlways",
+    "STLTimedAlways",
 ]
